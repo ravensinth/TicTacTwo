79663b70e7d433979962385c26529114da2c360a
ravensinth <visser.p.b@googlemail.com>
2014-02-03 22:43:18 +0100

    cursor start

--- TicTacTwo/TicTacTwo/Assets.cs
+++ TicTacTwo/TicTacTwo/Assets.cs
@@ -12,12 +12,14 @@ namespace TicTacTwo {
         public static Texture2D BoardBackground { get; set; }
         public static Texture2D Circle { get; set; }
         public static Texture2D Cross { get; set; }
+        public static Texture2D Cursor { get; set; }
 
 
         public static void LoadContent(ContentManager content) {
             BoardBackground = content.Load<Texture2D>("BoardBack");
             Circle = content.Load<Texture2D>("circle");
             Cross = content.Load<Texture2D>("cross");
+            Cursor = content.Load<Texture2D>("glove");
         }
     }
 }
---
+++ TicTacTwo/TicTacTwo/Cursor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace TicTacTwo {
+    public class Cursor {
+        int posX;
+        int posY;
+        Texture2D cursorImg;
+        public Cursor() {
+            cursorImg = Assets.Cursor;
+            posX= 0; 
+            posY = 0;
+        }
+    }
+}
